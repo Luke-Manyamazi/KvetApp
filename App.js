3875e1e2cc8b6b61c0437b94cdf0d34c5234e078
Luke-Manyamazi <lukemanymazi1@gmail.com>
2025-03-27 21:03:49 +0200

    initial commit

--- App.js
+++ App.js
@@ -8,5 +8,5 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,14 +5,52 @@ function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/background.jpg")}
+      source={require("../assets/baboon.jpeg")}
+      blurRadius={3}
     >
       <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Image
+          style={styles.logo}
+          source={require("../assets/kvet-logo.png")}
+        />
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            color: "#fff",
+            marginTop: 10,
+          }}
+        >
+          Kogelberg Villages
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            color: "#fff",
+            marginTop: 5,
+          }}
+        >
+          Environmental Trustees
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            color: "#fff",
+            marginTop: 100,
+          }}
+        >
+          Data Collection App
+        </Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.loginButton}>
+        <Text style={styles.buttonText}>Login</Text>
+      </View>
+      <View style={styles.registerButton}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </View>
     </ImageBackground>
   );
 }
@@ -23,10 +61,17 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
   loginButton: {
     width: "100%",
     height: "70",
     backgroundColor: "#fc5c65",
+    alignItems: "center",
+    justifyContent: "center",
   },
   logo: {
     width: "100",
@@ -41,6 +86,8 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "70",
     backgroundColor: "#4ecdc4",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
